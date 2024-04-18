f093b155907e8e5c6c7bffae9d53b38324e09ca7
Vanshika Sharma <70143660+vanshika-2008@users.noreply.github.com>
2024-04-18 19:00:15 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+app.use(express.json())
+
+var nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+    port: 465,
+    host: "smtp.gmail.com",
+    auth: {
+        user: '',
+        pass: '',
+    },
+    secure: true,
+});
+
+server.listen(process.env.PORT || 3030);
